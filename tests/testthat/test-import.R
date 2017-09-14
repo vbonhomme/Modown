d4a62e40bc2293baa2e879d093e65fd1eeef6db2
vbonhomme <bonhomme.vincent@gmail.com>
2017-09-14 09:00:48 +0200

    more tests

--- tests/testthat/test-import.R
+++ tests/testthat/test-import.R
@@ -4,4 +4,21 @@ test_that("import_coo1 works fine", {
   x <- import_coo1("3parts.coo")
   expect_true(is.list(x))
   expect_true(all(c("coo", "cov") %in% names(x)))
+  expect_true(is.list(x$coo))
+  expect_true(is.data.frame(x$cov))
+
+  x <- import_coo1("out.coo")
+  expect_true(is.list(x))
+  expect_true(all(c("coo", "cov") %in% names(x)))
+  expect_true(is.matrix(x$coo))
+  expect_true(is.data.frame(x$cov))
 })
+
+test_that("import_coo", {
+  x <- import_coo("olea.coo")
+  expect_true(is.list(x))
+  expect_true(all(sapply(x, function(.) is.matrix(.$coo))))
+  expect_true(all(sapply(x, function(.) is.data.frame(.$cov))))
+  expect_true(is.data.frame(do.call("rbind", lapply(x, function(.) .$cov))))
+})
+
