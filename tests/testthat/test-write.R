b28ae4703814a98ca957abd0cb3c5a682003ffaa
vbonhomme <bonhomme.vincent@gmail.com>
2017-09-16 01:05:47 +0200

    test-write

--- tests/testthat/test-write.R
+++ tests/testthat/test-write.R
@@ -1,28 +1,24 @@
 context("write")
 
-.test_write <- function(x="plop.coo"){
-  expect_true(file.exists(x))
-  silent <- file.remove(x)
+.test_write <- function(x){
+  f_rm <- function(){
+    if (file.exists("plop.coo"))
+      silent <- file.remove("plop.coo")
+  }
+
+  f_rm()
+  write(x, "plop.coo")
+  expect_true(file.exists("plop.coo"))
+  expect_error(write(x))
+  expect_silent(write(x, "plop.coo", force=TRUE))
+  expect_true(file.exists("plop.coo"))
+  f_rm()
+
 }
-
-.test_write_stop <- function(x="plop.coo"){
-  expect_error(file.exists(x))
-  silent <- file.remove(x)
-}
-
-f_cr <- function()
-  file.create("plop.coo")
-
-f_rm <- function()
-  file.remove("plop.coo")
 
 # library(Momocs)
 test_that("write", {
-  matrix(1:12, ncol=2) %>% write("plop.coo")
-  .test_write("plop.coo")
-  .f_cr()
-  matrix(1:12, ncol=2) %>% write("plop.coo")
-  .f_rm()
+  matrix(1:12, ncol=2) %>% .test_write
 })
 
 
